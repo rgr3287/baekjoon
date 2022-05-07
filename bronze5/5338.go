88df9140fce6c2bc32a6322c062750a6cf3a00aa
곽현근 <48711195+rgr3287@users.noreply.github.com>
2022-05-07 19:22:28 +0900

    출력 형식

---
+++ bronze5/5338.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("       _.-;;-._")
+	fmt.Println("'-..-'|   ||   |")
+	fmt.Println("'-..-'|_.-;;-._|")
+	fmt.Println("'-..-'|   ||   |")
+	fmt.Println("'-..-'|_.-''-._|")
+}
+
+// 출력형식 문제 Bronze 5 = 5338
