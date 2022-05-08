e6ba0c34b3dd36f95ceb4868b2a730b9267f6358
곽현근 <48711195+rgr3287@users.noreply.github.com>
2022-05-08 15:50:25 +0900

    출력문제

---
+++ bronze5/5339.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("     /~\\")
+	fmt.Println("    ( oo|")
+	fmt.Println("    _\\=/_")
+	fmt.Println("   /  _  \\")
+	fmt.Println("  //|/.\\|\\\\")
+	fmt.Println(" ||  \\ /  ||")
+	fmt.Println("============")
+	fmt.Println("|          |")
+	fmt.Println("|          |")
+	fmt.Println("|          |")
+}
+
+// 백준 bronze5 5339문제
