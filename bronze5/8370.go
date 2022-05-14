8d0dd2c07ffee7bf4574d8eb0c76b7154effaa56
곽현근 <48711195+rgr3287@users.noreply.github.com>
2022-05-14 20:23:24 +0900

    입력값에 따른 출력 문제

---
+++ bronze5/8370.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n1, k1, n2, k2 uint
+	fmt.Scanf("%d %d %d %d", &n1, &k1, &n2, &k2)
+	fmt.Println(n1*k1 + n2*k2)
+}
+
+// 입력값과 출력값을 확인하면 금방 풀 수 있는 문제
