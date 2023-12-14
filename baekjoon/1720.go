3860e00c73a74f2847f69ed6cf7d7e20401eaf92
hyeongeun gwak <hggwak@grooo.co.jp>
2023-12-14 17:16:40 +0900

    1720: 타일코드

---
+++ baekjoon/1720.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+var dp [32]int64
+var sym [32]int64
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Println(tileCode(n))
+}
+
+func tileCode(n int) int64 {
+	return (tile(n)-symmetry(n))/2 + symmetry(n)
+}
+
+func tile(n int) int64 {
+	if dp[n] != 0 {
+		return dp[n]
+	}
+	if n == 1 {
+		dp[1] = 1
+		return dp[1]
+	}
+	if n == 2 {
+		dp[2] = 3
+		return dp[2]
+	}
+
+	dp[n] = tile(n-1) + 2*tile(n-2)
+
+	return dp[n]
+}
+
+func symmetry(n int) int64 {
+	if sym[n] != 0 {
+		return sym[n]
+	}
+	if n == 1 {
+		sym[1] = 1
+		return sym[1]
+	}
+	if n == 2 {
+		sym[2] = 3
+		return sym[2]
+	}
+	if n == 3 {
+		sym[3] = 1
+		return sym[3]
+	}
+	if n == 4 {
+		sym[4] = 5
+		return sym[4]
+	}
+
+	sym[n] = symmetry(n-2) + 2*symmetry(n-4)
+
+	return sym[n]
+}
